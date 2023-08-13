5f92cc0f9899db40dfc443d9cc249b58af0912be
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2023-08-13 18:22:29 +0800

    Make caseless parameter non optional

--- src/search.h
+++ src/search.h
@@ -16,7 +16,7 @@ template <typename ForwardSearcher>
 std::vector<size_t>
 search_all(ForwardSearcher forward_searcher, std::string_view file_contents,
            std::string_view pattern, size_t beginning_offset,
-           size_t ending_offset, bool caseless = false) {
+           size_t ending_offset, bool caseless) {
     std::vector<size_t> out;
     while (true) {
         size_t offset = forward_searcher(
@@ -35,7 +35,7 @@ template <typename ForwardSearcher>
 size_t search_forward_n(ForwardSearcher forward_searcher, size_t num_repeats,
                         std::string_view file_contents,
                         std::string_view pattern, size_t beginning_offset,
-                        size_t ending_offset, bool caseless = false) {
+                        size_t ending_offset, bool caseless) {
     size_t latest_hit = std::string::npos;
     for (size_t i = 0; i < num_repeats; ++i) {
         size_t result = forward_searcher(
@@ -53,7 +53,7 @@ template <typename BackwardSearcher>
 size_t search_backward_n(BackwardSearcher backward_searcher, size_t num_repeats,
                          std::string_view file_contents,
                          std::string_view pattern, size_t beginning_offset,
-                         size_t ending_offset, bool caseless = false) {
+                         size_t ending_offset, bool caseless) {
     size_t latest_hit = std::string::npos;
     for (size_t i = 0; i < num_repeats; ++i) {
         size_t result = backward_searcher(
@@ -69,16 +69,16 @@ size_t search_backward_n(BackwardSearcher backward_searcher, size_t num_repeats,
 
 size_t basic_search_first(std::string_view file_contents,
                           std::string_view pattern, size_t beginning_offset,
-                          size_t ending_offset, bool caseless = false);
+                          size_t ending_offset, bool caseless);
 
 size_t basic_search_last(std::string_view file_contents,
                          std::string_view pattern, size_t beginning_offset,
-                         size_t ending_offset, bool caseless = false);
+                         size_t ending_offset, bool caseless);
 
 size_t regex_search_first(std::string_view file_contents,
                           std::string_view pattern, size_t beginning_offset,
-                          size_t ending_offset, bool caseless = false);
+                          size_t ending_offset, bool caseless);
 
 size_t regex_search_last(std::string_view file_contents,
                          std::string_view pattern, size_t beginning_offset,
-                         size_t ending_offset, bool caseless = false);
+                         size_t ending_offset, bool caseless);
